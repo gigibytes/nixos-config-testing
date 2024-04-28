e7f37d7b8da6e474ef8cb68a066405a888891f09
GiGi Hurtault <gigi@hurtault.dev>
2024-04-27 21:13:47 -0400

    correct home.nix location

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
 	    home-manager.useGlobalPkgs = true;
 	    home-manager.useUserPackages = true;
 
-	    home-manager.users.gigi = import ./home.nix;
+	    home-manager.users.gigi = import ./flakes/home.nix;
 
 	  }
         ];
