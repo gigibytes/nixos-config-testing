9a9f658d8ab4f19505f72e6a90696c7489b987f1
GiGi Hurtault <gigi@hurtault.dev>
2024-04-27 21:12:24 -0400

    add home-manager input and module

--- flake.nix
+++ flake.nix
@@ -4,17 +4,34 @@
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     # Please replace my-nixos with your hostname
-    nixosConfigurations.nixos-testing = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
+    nixosConfigurations = { 
+      nixos-testing = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
-        ./configuration.nix
-      ];
+          ./configuration.nix
+
+	  # Home Manager Module
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+
+	    home-manager.users.gigi = import ./home.nix;
+
+	  }
+        ];
+      };
     };
   };
 }
