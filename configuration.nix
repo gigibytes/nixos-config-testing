7caf804e24656b9c17bd7726ef32e615ffc5d68b
gigi <gigi@hurtault.dev>
2024-05-15 20:37:19 -0400

    add fish

--- configuration.nix
+++ configuration.nix
@@ -136,6 +136,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    fish
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
