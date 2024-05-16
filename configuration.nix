1fb140b17527fff57f223253d661685db3405774
gigi <gigi@hurtault.dev>
2024-05-15 20:41:34 -0400

    add fish differently + plugin. remove firefox

--- configuration.nix
+++ configuration.nix
@@ -124,8 +124,7 @@
     nerdfonts
   ];
 
-  # Install firefox.
-  programs.firefox.enable = true;
+  programs.fish.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -136,7 +135,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    fish
+    fishPlugins.pure
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
